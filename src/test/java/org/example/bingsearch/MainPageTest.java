5e95c87bdb1143ed5573c4c3b84ac8b707007e29
yar92zer <yar199199@gmail.com>
2024-07-28 16:06:54 +0400

    Работа со списками

--- src/test/java/org/example/bingsearch/MainPageTest.java
+++ src/test/java/org/example/bingsearch/MainPageTest.java
@@ -1,16 +1,21 @@
 package org.example.bingsearch;
 
-import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainPageTest {
     private WebDriver driver;
@@ -18,7 +23,6 @@ public class MainPageTest {
     @BeforeEach
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
-        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
@@ -40,5 +44,20 @@ public class MainPageTest {
 
         WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
         assertEquals(input, searchPageField.getAttribute("value"));
+        List<WebElement> results = driver.findElements(By.cssSelector("h2 > a[href]"));
+        clickElement(results, 0);
+        driver.getCurrentUrl();
+
+        ///assertEquals
+    }
+
+    public void clickElement(List<WebElement> results, int num) {
+        results.get(num).click();
+        System.out.println("Клик по номеру 0 поиска");
+        By button = By.cssSelector("#Layer_1");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+        WebElement disButton = driver.findElement(button);
+        assertTrue(disButton.isEnabled(), "Кнопка нет после 5 сек");
     }
 }
